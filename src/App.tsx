0cb92fbe4efa60fc219a4396670a20e9ae61c501
Ramal Abbassov <ramalabbasov044@gmail.com>
2024-04-12 16:37:23 +0400

    start folder structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from './components/Static/index'
+import './assets/css/reset.css'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Static/Header/index.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Header = () => {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  )
+}
+
+export default Header
+
+const Wrapper = styled.header`
+  
+`
