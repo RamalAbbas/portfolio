390261e28f43555deafec897f1999676300b176e
Ramal Abbassov <ramalabbasov044@gmail.com>
2024-04-18 22:56:25 +0400

    update social media links

--- src/components/Static/SocialMedia/index.tsx
+++ src/components/Static/SocialMedia/index.tsx
@@ -1,17 +1,15 @@
-import { useNavigate } from "react-router-dom"
 import styled from "styled-components"
 
 import { DarkGithub , Linkedln } from "../../Icons"
 
 const SocialMedia = () => {
-    const navigate = useNavigate()
     return (
         <Wrapper>
-            <Github onClick={() => navigate("https://github.com/ramalabbasov044")}>
+            <Github onClick={() => window.open("https://github.com/ramalabbasov044", "_blank")}>
                 <DarkGithub/>
             </Github>
 
-            <LinkedlnBox onClick={() => navigate("https://www.linkedin.com/in/ramal-abbasov-504b35242/")}>
+            <LinkedlnBox onClick={() => window.open("https://www.linkedin.com/in/ramal-abbasov-504b35242/", "_blank")}>
                 <Linkedln />
             </LinkedlnBox>
         </Wrapper>
